4aa55af4800166f28c811e1be916349d7bab55dd
Noseary <reason.yx@gmail.com>
2020-10-13 00:45:26 +0800

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(FriendlyChatApp());
+
+const APP_NAME = 'Start up name generator';
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: APP_NAME,
+      home: ChartScreen(),
+    );
+  }
+}
+
+class ChartScreen extends StatefulWidget {
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+                child: GestureDetector(
+              onTap: () => {
+                print('tapped'),
+              },
+            )),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 14.0, vertical: 5.0),
+            padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 5.0),
+            decoration: BoxDecoration(
+              border: Border(
+                  top: BorderSide(
+                      width: 1.0, color: Theme.of(context).accentColor)),
+            ),
+            child: Row(
+              children: <Widget>[
+                // Expanded?
+                Flexible(
+                  child: TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Send a message',
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: const Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text),
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    // return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(title: Text(APP_NAME)),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
